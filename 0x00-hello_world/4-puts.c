24487095d64b31a9b1707c17e346967544dd611c
Uche14 <martinsobike7@gmail.com>
2022-09-08 19:30:01 -0700

    Sound

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,13 @@
 #include <stdio.h>
 
-
-
 /**
-
-* main - Printing
-
+* main - Print out
 *
-
 * Return: Always 0 (Successful)
-
 */
-
 int main(void)
-
 {
-
 puts("\"Programming is like building a multilingual
-
 puzzle");
-
 return (0);
-
 }
