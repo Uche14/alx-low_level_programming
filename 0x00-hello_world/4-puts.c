adeed4f055a288deab1c1a819effab3385230538
Uche14 <martinsobike7@gmail.com>
2022-09-08 19:45:43 -0700

    Lucid

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,14 @@
 /**
 * main - Print out
 *
-* Return: Always 0 (Successful)
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
 */
 int main(void)
 {
 puts("\"Programming is like building a multilingual
+
 puzzle");
 return (0);
 }
